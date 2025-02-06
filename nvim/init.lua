31f314c857a1023f75ef99616620f10126876a13
Grace Yoder <yoder177@data.cs.purdue.edu>
2025-02-06 14:40:36 -0500

    eastwood linting

--- nvim/init.lua
+++ nvim/init.lua
@@ -7,11 +7,17 @@ vim.g.hostname = hostname
 
 vim.cmd("source ~/.vim/autoload/plug.vim")
 vim.cmd("source ~/.vimrc")
-vim.env.PATH = vim.env.PATH .. ':~/clangd_19.1.2/bin'
+vim.env.PATH = vim.env.PATH .. ':/u/riker/u99/yoder177/clangd_19.1.2/bin:/u/riker/u98/cs240/bin'
 
-
-
+-- vim.cmd("source ~/.config/nvim/lua/cmp-config.lua")
+require("cmp-config")
+capabilities = require('cmp_nvim_lsp').default_capabilities()
 require('lspconfig').clangd.setup({
-  on_attach = custom_attach_clangd,
-  cmd = { "/homes/yoder177/clangd_19.1.2/bin/clangd", "--clang-tidy" }
+  -- on_attach = require("completion").on_attach
+  capabilities = capabilities, 
+  cmd = {
+    "clangd",
+    "--clang-tidy",  -- Enable clang-tidy
+    "--background-index",
+  }  
 })
