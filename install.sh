91c12c0d2e02b3bb859268709a49cbbd349211ab
gyoder <70408179+gyoder@users.noreply.github.com>
2025-02-06 15:25:05 -0500

    fixed install script

--- install.sh
+++ install.sh
@@ -5,7 +5,6 @@ git clone https://github.com/gyoder/.vim.git $HOME/.vim
 
 # Create a symlink to the .vimrc file and /.config/nvim 
 ln -s $HOME/.vim/.vimrc $HOME/.vimrc
-# link the .config/nvim/ folder to .vim/nvim/
 ln -s $HOME/.vim/nvim $HOME/.config/nvim
 
 # Install vim-plug plugins
