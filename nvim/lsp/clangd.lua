8126022f974d986dd6e0a6eaaf6886faad092bf4
gyoder <70408179+gyoder@users.noreply.github.com>
2025-04-08 14:47:40 -0400

    pio clangd fix

--- nvim/lsp/clangd.lua
+++ nvim/lsp/clangd.lua
@@ -5,17 +5,15 @@ return {
     '--background-index',
     '--offset-encoding=utf-8',
   },
-  filetypes = { 'c', 'cpp', 'objc', 'objcpp', 'cuda', 'proto' },
-  root_dir = function(fname)
-    return util.root_pattern(
-      '.clangd',
-      '.clang-tidy',
-      '.clang-format',
-      'compile_commands.json',
-      'compile_flags.txt',
-      'configure.ac' -- AutoTools
-    )(fname) or vim.fs.dirname(vim.fs.find('.git', { path = fname, upward = true })[1])
-  end,
+  filetypes = { 'c', 'cpp', 'objc', 'objcpp', 'cuda', 'proto', 'ino' },
+  root_markers = {
+    '.clangd',
+    '.clang-tidy',
+    '.clang-format',
+    'compile_commands.json',
+    'compile_flags.txt',
+    'configure.ac' -- AutoTools
+  },
   single_file_support = true,
   capabilities = {
     textDocument = {
