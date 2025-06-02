b045bc22234dd6c9517ae6149aa358e66126fa7e
gyoder <70408179+gyoder@users.noreply.github.com>
2025-06-02 10:23:05 -0600

    stuff

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -141,7 +141,9 @@ return require('packer').startup(function(use)
   use 'theHamsta/nvim-dap-virtual-text'
   use { "rcarriga/nvim-dap-ui", requires = {"mfussenegger/nvim-dap", "nvim-neotest/nvim-nio"} }
 
+  use 'wakatime/vim-wakatime'
 
+  use { "geigerzaehler/tree-sitter-jinja2" }
 
 
 
